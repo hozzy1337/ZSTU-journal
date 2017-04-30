a47398ba721a2bd58d0d8253927d09f9a1dfc97e
hozzy <hozzy1337@gmail.com>
2017-04-30 18:41:31 +0300

    Добавьте файлы проекта.

---
+++ ZSTU journal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZSTU_journal.MvcApplication" Language="C#" %>
---
+++ ZSTU journal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZSTU_journal.Startup))]
+namespace ZSTU_journal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
